93a28c457e741ede10ad40893a87ea06ff739586
David Alejandro Serrada Botía <davidasb.developer@gmail.com>
2022-05-20 03:15:16 -0400

    feat: :sparkles: add function to create HTML template

--- utils/sendEmail.go
+++ utils/sendEmail.go
@@ -4,10 +4,18 @@ import (
 	"net/smtp"
 )
 
+const (
+	MIME = "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+)
 
-func SendEmail() error {
+func SendEmail(data CalculatedData) error {
+	body, err := CreateEmailTemplate(data)
+	body = "Subject: Personal Transactions\n" + MIME + "\r\n" + body
+	if err != nil {
+		return err
+	}
 
-	err := smtp.SendMail("smtp.gmail.com:587", smtp.PlainAuth("", "testsenderstori@gmail.com", "Hola123.", "smtp.gmail.com"),"", []string{"davidasb.developer@gmail.com"}, []byte("dummy message"))
+	err = smtp.SendMail("smtp.gmail.com:587", smtp.PlainAuth("", "testsenderstori@gmail.com", "Hola123.", "smtp.gmail.com"),"", []string{"davidasb.developer@gmail.com"}, []byte(body))
 
 	if err != nil {
 		return err
